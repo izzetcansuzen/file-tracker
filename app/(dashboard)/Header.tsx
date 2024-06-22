334c5f85cdf21a9c9b42df6c09bccf8579864f4e
izzetcansuzen <izzetcansuzen@gmail.com>
2024-06-22 14:47:03 +0300

    Added border to header

--- app/(dashboard)/Header.tsx
+++ app/(dashboard)/Header.tsx
@@ -7,8 +7,10 @@ import Sidebar from "@/app/(dashboard)/Sidebar"
 import {Menu} from "lucide-react"
 
 export default function Header(){
+    /*TODO: profile image alanı eklenmeli (burası clerk'mü olacak?)*/
+    /*TODO: search alanı eklenmeli*/
     return (
-        <nav className='lg:ml-56 h-14 flex items-center justify-between px-4'>
+        <nav className='lg:ml-56 h-14 flex items-center justify-between px-4 border-b-2'>
             <div className='flex lg:hidden'>
                 <Sheet>
                     <SheetTrigger><Menu /></SheetTrigger>
--- app/(dashboard)/SidebarContent.tsx
+++ app/(dashboard)/SidebarContent.tsx
@@ -2,11 +2,15 @@ import {Button} from "@/components/ui/button";
 import {File} from "lucide-react";
 import Link from "next/link";
 
+/*TODO: Sidebar bilgileri eklenmeli*/
 const sideBarItems = {
 
 }
 
 export default function SidebarContent(){
+    /*TODO: Linkleri componentleştir*/
+    /*TODO: Linkleri dinamikleştir*/
+    /*TODO: Iconları nasıl dinamikleştirebiliriz?*/
     return (
         <div className='grid gap-2'>
             <Link href='/'>
