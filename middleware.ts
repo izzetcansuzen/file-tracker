efbd3b72dc39daf1cfc1415981ec14f476019ab7
izzetcansuzen <izzetcansuzen@gmail.com>
2024-06-22 15:09:38 +0300

    Completed Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+    '/(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (!auth().userId && isProtectedRoute(req)) {
+        return auth().redirectToSignIn();
+    }
+});
 
 export const config = {
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
